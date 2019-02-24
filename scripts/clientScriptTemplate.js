caba177b4e53355b042651bcc61c7ec21a1ce214
Simon <Simon@LAPTOP-NAT2JJES.Speedport_W_921V_1_45_000>
2019-02-24 15:37:11 +0100

    Added server and client script templates.

---
+++ scripts/clientScriptTemplate.js
@@ -0,0 +1,13 @@
+let clientSystem = client.registerSystem(0, 0);
+
+clientSystem.initialize = function() {
+	// Initialize all the things here...
+};
+
+clientSystem.update = function() {
+	// Update all the things here...
+};
+
+clientSystem.shutdown = function() {
+	// Cleanup all the things here...
+};
---
+++ scripts/serverScriptTemplate.js
@@ -0,0 +1,13 @@
+let serverSystem = server.registerSystem(0, 0);
+
+serverSystem.initialize = function() {
+	// Initialize all the things here...
+};
+
+serverSystem.update = function() {
+	// Update all the things here...
+};
+
+serverSystem.shutdown = function() {
+	// Cleanup all the things here...
+};
